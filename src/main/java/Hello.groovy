f52f3699f5ec07387360e56882e66ceaef3fa00b
李耀华 <liyaohua.mail@gmail.com>
2020-05-23 11:19:31 +0800

    we

--- src/main/java/Hello.groovy
+++ src/main/java/Hello.groovy
@@ -1,10 +1,3 @@
-/**
- * @version V2.0* @title: Hello* @package:
- * @description:
- * @author: 李耀华* @date: 2020-05-2311:09
- * @Copyright: 2019 www.zielsmart.com Inc. All rights reserved.
- * 注意：本内容仅限于郑州致欧网络科技有限公司内部传阅，禁止外泄以及用于其他的商业目的
- */
 int add(int a,int b){
     return a+b;
 }
