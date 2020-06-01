05a4c3a37221da0aed08eec1bb09b86dab2d0121
李耀华 <liyaohua.mail@gmail.com>
2020-06-01 13:04:26 +0800

    we

--- src/main/java/Hello.groovy
+++ src/main/java/Hello.groovy
@@ -1,22 +1,15 @@
 
+import com.zielsmart.zeus.net.model.NetRequest
+
 int add(int a,int b){
     return a+b;
 }
 void logInfo(){
-    println(add(1,2))
-    println("--下面是通过调用Java输出日志----")
-   // zeusLog.info("我是Zeus调用的日志:"+add(2,3))
-    println("---下面是调用Java方法")
-   //helloJob.run(null)
 
-    List<Integer> list = new ArrayList<>();
-    for (int i=0;i<10;i++) {
-        list.add(i);
-    }
-
-   for(Integer i:list){
-       println(i)
-   }
+    NetRequest netRequest = new NetRequest();
+    netRequest.setUrl("http://wb.ziel.cn")
+    String body = zeusHttp.get(netRequest).getResponse().body();
+    zeusLog.info(body)
 }
 
 logInfo()
