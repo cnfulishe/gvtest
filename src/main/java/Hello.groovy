2e66e413c8e9e163309cd1a35707c40c4dcf7ffb
李耀华 <liyaohua.mail@gmail.com>
2020-06-01 13:19:28 +0800

    we微软

--- src/main/java/Hello.groovy
+++ src/main/java/Hello.groovy
@@ -1,12 +1,17 @@
+import cn.hutool.json.JSONUtil
 import com.zielsmart.zeus.net.http.ZeusHttpTemplate
 import com.zielsmart.zeus.net.model.NetRequest
+import com.zielsmart.zeus.net.model.NetResponse
 import com.zielsmart.zeus.net.model.ProxyType
 void logInfo(){
 
     NetRequest netRequest = new NetRequest();
     ZeusHttpTemplate zeusHttpTemplate = new ZeusHttpTemplate();
+
     netRequest.setUrl("https://www.baidu.com").setProxyType(ProxyType.NO);
-    zeusHttp.get(netRequest).getResponse()
+
+    NetResponse response = zeusHttpTemplate.get(netRequest).getResponse();
+    zeusLog.info(JSONUtil.toJsonStr(response))
     zeusLog.info("我是一个测试");
 }
 logInfo()
