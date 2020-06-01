871050ce0b304b430aac89e610b2dfda10ac9a17
李耀华 <liyaohua.mail@gmail.com>
2020-06-01 13:10:03 +0800

    we

--- src/main/java/Hello.groovy
+++ src/main/java/Hello.groovy
@@ -5,8 +5,8 @@ import com.zielsmart.zeus.net.model.ProxyType
 void logInfo(){
 
     NetRequest netRequest = new NetRequest();
-    netRequest.setUrl("http://wb.ziel.cn").setProxyType(ProxyType.NO)
+    netRequest.setUrl("http://wb.ziel.cn").setProxyType(ProxyType.NO);
     String body = zeusHttp.get(netRequest).getResponse().body();
-    zeusLog.info(body)
+    zeusLog.info(body);
 }
 logInfo()
