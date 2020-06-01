12b10f4751974eff9928bcf3dd751e4bfb1bc605
李耀华 <liyaohua.mail@gmail.com>
2020-06-01 12:58:27 +0800

    we

--- src/main/java/Hello.groovy
+++ src/main/java/Hello.groovy
@@ -1,5 +1,22 @@
+
 int add(int a,int b){
     return a+b;
 }
-println(add(1,2))
-zeusLog.log("我是Zeus调用的日志:"+add(2,3))
+void logInfo(){
+    println(add(1,2))
+    println("--下面是通过调用Java输出日志----")
+   // zeusLog.info("我是Zeus调用的日志:"+add(2,3))
+    println("---下面是调用Java方法")
+   //helloJob.run(null)
+
+    List<Integer> list = new ArrayList<>();
+    for (int i=0;i<10;i++) {
+        list.add(i);
+    }
+
+   for(Integer i:list){
+       println(i)
+   }
+}
+
+logInfo()
